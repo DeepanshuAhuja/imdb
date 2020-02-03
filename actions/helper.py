ff21934a94cf9a8ce1922ae536ef7e3ca459cfb0
DeepanshuAhuja <ahujadeepanshu494@gmail.com>
2020-02-03 14:34:57 +0530

    Update helper.py

--- actions/helper.py
+++ actions/helper.py
@@ -1,6 +1,13 @@
 class Helper():
 
     def super_data(self,superlist):
+        
+        """
+        Prepare the golden for all the movies 
+        
+        {<ranking>:[<movie_name>,<release_date>,<imdb_ratings>]}
+        
+        """
         dict1 = {}
 
         for ele in superlist:
@@ -12,6 +19,14 @@ class Helper():
 
 
     def write_into_file(self,filename,superlist):
+        
+        """
+        Function used to write the data into the file.
+        
+        parameters :-
+            filename:- Name of the file in which you have to write
+            superlist:- list of string that you have to write in the file
+        """
         fo = open(filename, "w")
         for ele in superlist:
             fo.write(ele + "\n")
